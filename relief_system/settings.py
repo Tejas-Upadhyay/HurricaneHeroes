caa9d41939f9cee4bedf646341026d95d10309e3
Upadhyay, Tejas <tejasupadhyayfl@gmail.com>
2026-08-16 10:50:21 -0400

    Added ALLOWED_HOSTS

--- relief_system/settings.py
+++ relief_system/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-relief-management-system-dev-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
